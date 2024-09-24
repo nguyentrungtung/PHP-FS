63893cba8f690d7f159685dd140d0005effaf1f8
unknown <phamvannam.dev@gmail.com.com>
2024-09-24 15:14:10 +0700

    add layout admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', [AdminController::class, 'index'])->name('admin.dashboard');
+
+
