d25e5dd3d5c919b1036fcddbe3d2c37fe2d4742c
Tran Cong Danh <danh.trancong10702@gmail.com>
2024-10-01 22:49:46 +0700

    .

---
+++ app/Models/District.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class District extends Model
+{
+    use HasFactory;
+    // 
+    protected $fillable = [
+        'district_name',
+        'city_id'
+    ];
+    // 
+    public function city(){
+        return $this->belongsTo(City::class);
+    }
+    // 
+    public function town(){
+        return $this->hasMany(Town::class);
+    }
+}
